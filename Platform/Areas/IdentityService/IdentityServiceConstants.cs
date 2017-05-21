18352a0b4ed6a2a373715aaccdd265fd6963e21a
Vitaly Shvetsov <nosferatu2995@mail.ru>
2017-05-21 19:27:22 +0700

    Add project files.

---
+++ Platform/Areas/IdentityService/IdentityServiceConstants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.Service.Extensions;
+
+namespace Platform.Identity
+{
+    // These values are used to setup the identity servcie and should not be changed
+    public class IdentityServiceConstants
+    {
+        // Name of the single identity service tenant
+        public const string Tenant = "IdentityService";
+        
+        // Unique ID of the single identity service tenant
+        public const string TenantId = "AA2F6B15-A89C-4E1A-ACF7-8639CD1443B5";
+
+        // Default policy for the identity service
+        public const string DefaultPolicy = "signinsignup";
+
+        // Identity service version
+        public const string Version = "2.0";
+
+        // Identity service token version
+        public const string TokenVersion = "1.0";
+    }
+}
